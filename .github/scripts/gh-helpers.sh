f63ef4899380b8cdc2e1e88a7301ca657645f3cc
callmetango <callmetango@users.noreply.github.com>
2026-07-21 16:53:44 +0000

    Refactored build scripts for release branches

--- .github/scripts/gh-helpers.sh
+++ .github/scripts/gh-helpers.sh
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: AGPL-3.0-only
-# Copyright (C) 2026 - callmetango for SonicDE.org
+# SPDX-FileCopyrightInfo: 2026 callmetango for XLibre
 
 # Arguments
 # $1: name of the env variable
 # $2: value of the env variable
 gh_env_set() {
 	printf "$1=%s\n" "$2" >> $GITHUB_ENV
+	export "$1"="$2"
 }
